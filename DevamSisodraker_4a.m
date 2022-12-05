23ccc936965928d3c049e32c9643ddfa86644b2b
Devam Sisodraker <devam@student.ubc.ca>
2022-12-05 15:13:39 -0800

    4a is done

---
+++ DevamSisodraker_4a.m
@@ -0,0 +1,46 @@
+gcf
+hold on;
+
+clear;
+load('powerMatrix.mat')
+
+k = 0;
+vector_k = randn(100, 1);
+lambda_k = transpose(vector_k)*A*vector_k;
+lambda_k_1 = transpose(vector_k)*A*vector_k;
+lambda_audit = [];
+lambda_delta_audit = [];
+eigval = max(eig(A));
+
+while true
+    % statements here
+    % if ~WhileCondition, break ; end
+    lambda_k_1 = lambda_k;
+    vector_k = A*vector_k;
+    vector_k = vector_k/norm(vector_k);
+    lambda_k = transpose(vector_k)*A*vector_k;
+    lambda_audit = [lambda_audit, lambda_k];
+    lambda_delta_audit = [lambda_delta_audit, abs(lambda_k - eigval)];
+    k = k + 1;
+    if abs(lambda_k_1 - lambda_k) < 10^-4
+        break;
+    end
+end
+
+plot(1:1:k, lambda_audit);
+
+plot(1:1:k, lambda_delta_audit);
+
+plot([1, k], [eigval, eigval]);
+
+title("4a");
+legend({ ...
+    '\lambda_k', ...
+    '| \lambda_k - \lambda_{MAX} |', ...
+    '\lambda_{MAX}', ...
+});
+xlabel("k");
+ylabel("Value");
+
+hold off;
+saveas(gcf, "DevamSisodraker_4a.jpg", "jpg");
