6b2bd23bb7dfd90e23ca21a272be5da87dc86c6c
Devam Sisodraker <devam@student.ubc.ca>
2022-12-05 15:35:27 -0800

    4b done

--- DevamSisodraker_4b.m
+++ DevamSisodraker_4b.m
@@ -2,26 +2,32 @@ gcf
 hold on;
 
 clear;
-load('powerMatrix.mat')
+load('powerMatrix.mat');
 
 k = 0;
 vector_k = randn(100, 1);
+vector_k_1 = vector_k;
 lambda_k = transpose(vector_k)*A*vector_k;
 lambda_k_1 = transpose(vector_k)*A*vector_k;
 lambda_audit = [];
 lambda_delta_audit = [];
 eigval = max(eig(A));
+alpha = 4;
 
 while true
     % statements here
     % if ~WhileCondition, break ; end
     lambda_k_1 = lambda_k;
-    vector_k = A*vector_k;
+    vector_k_1 = vector_k;
+
+    vector_k = (A - alpha * eye(size(A)))/transpose(vector_k_1);
     vector_k = vector_k/norm(vector_k);
     lambda_k = transpose(vector_k)*A*vector_k;
+
     lambda_audit = [lambda_audit, lambda_k];
     lambda_delta_audit = [lambda_delta_audit, abs(lambda_k - eigval)];
     k = k + 1;
+  
     if abs(lambda_k_1 - lambda_k) < 10^-4
         break;
     end
@@ -33,7 +39,7 @@ plot(1:1:k, lambda_delta_audit);
 
 plot([1, k], [eigval, eigval]);
 
-title("4a");
+title("4b");
 legend({ ...
     '\lambda_k', ...
     '| \lambda_k - \lambda_{MAX} |', ...
@@ -43,5 +49,5 @@ xlabel("k");
 ylabel("Value");
 
 hold off;
-saveas(gcf, "DevamSisodraker_4a.jpg", "jpg");
+saveas(gcf, "DevamSisodraker_4b.jpg", "jpg");
 lambda_k
